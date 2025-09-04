0af6722fbc378b300ce12e4ae30b30cd00695acd
Rajdeep Ghosh <grajdeep2000@gmail.com>
2025-09-04 23:42:15 +0530

    Fixed build issues

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  presets: ["next/babel"],
-  plugins:
-    process.env.NODE_ENV === "production" ? ["transform-remove-console"] : [],
-};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    // Strip console.* only in production
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? { exclude: ["error", "warn"] } // keep error/warn if you want
+        : false,
+  },
 };
 
 export default nextConfig;
